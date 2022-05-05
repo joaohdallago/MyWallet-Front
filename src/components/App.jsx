afdc6728a3316633fff4cf4a4d8a476b10b9affe
joaohdallago <joaohdallago@gmail.com>
2022-05-05 10:37:00 -0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import GlobalStyles from '../theme/globalStyles';
+
+export default function App() {
+	return (
+		<>
+			<GlobalStyles />
+      lala
+		</>
+	);
+}
---
+++ src/theme/globalStyles.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+
+        padding: 0;
+        border: 0;
+        margin: 0;
+    }
+
+    body {
+        background-color: #8C11BE;
+    }
+`
+
+export default GlobalStyles;
+
+// font-family: 'Raleway', sans-serif;
+// font-family: 'Saira Stencil One', cursive;
